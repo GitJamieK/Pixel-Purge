9491aeab1b80a29bd9ff31a37c803f8506653023
JamieK <koflerjamie@gmail.com>
2026-03-04 00:00:31 +0100

    push mainmenu to actionstack

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,11 +1,29 @@
+using ActionStack;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-    public void Play() {
-        SceneManager.LoadScene("Game");
-    }
-    public void Quit() {
-        Application.Quit();
+namespace Game {
+
+    public class MainMenu : ActionStack.ActionStack.ActionBehavior {
+
+        private void Start() {
+            ActionStack.ActionStack.Main.PushAction(this);
+        }
+
+        public override bool IsDone() {
+            return false;
+        }
+         
+
+
+
+
+
+        public void Play() {
+            SceneManager.LoadScene("Game");
+        }
+        public void Quit() {
+            Application.Quit();
+        }
     }
 }
